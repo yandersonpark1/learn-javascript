b3375779a2eba82425e7ae66ac1f0e6941791150
yandersonpark1 <everydayap3@gmail.com>
2025-10-15 22:27:44 -0400

    reedit 1.21

--- 1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ 1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,15 +1,10 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById('welcome-el')
+let welcomeEl = document.getElementById("welcome-el")
 // Create two variables (name & greeting) that contains your name
 // and the greeting we want to render on the page
-let name = Anderson 
+let name = "Anderson" 
 let greeting = "Welcome Back"
 // Render the welcome message using welcomeEl.innerText
-function render() {
-    let message = name + greeting
-    welcomeEl.innerText = message
-    console.log(message)
-    return
-}
 
-render()
+let message = greeting + name
+welcomeEl.innerText = message
