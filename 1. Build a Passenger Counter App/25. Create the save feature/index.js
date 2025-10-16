9565d445301685700a0b8086e1bef371ff6e54d1
yandersonpark1 <everydayap3@gmail.com>
2025-10-15 22:40:31 -0400

    completed 1.25

--- 1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ 1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -1,6 +1,9 @@
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
+
 let countEl = document.getElementById("count-el")
 let count = 0
+let saveEl = document.getElementById('save-el')
+
 
 function increment() {
     count += 1
@@ -9,7 +12,9 @@ function increment() {
 
 function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countStr = count + " - "
     // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText += countStr
     // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
 }
