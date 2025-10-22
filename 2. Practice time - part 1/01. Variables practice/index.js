3eef37a441c82cb5ba67fcb267257a7419befa56
yandersonpark1 <everydayap3@gmail.com>
2025-10-22 00:53:53 -0400

    2.1 complete

--- 2. Practice time - part 1/01. Variables practice/index.js
+++ 2. Practice time - part 1/01. Variables practice/index.js
@@ -1,7 +1,8 @@
 // Create two variables, firstName and lastName
-
+let firstName = "Anderson"
+let lastName = "Park"
 // Concatenate the two variables into a third variable called fullName
-
+let fullName = firstName + " " + lastName
 // Log fullName to the console
 
-
+console.log(fullName)
