c97a15a8dad854504d69227b9267fb7d501f3319
yandersonpark1 <everydayap3@gmail.com>
2025-10-22 22:54:43 -0400

    3.2-3.16 complete

--- 3. Build a Blackjack game/03. If...else conditionals/index.js
+++ 3. Build a Blackjack game/03. If...else conditionals/index.js
@@ -2,3 +2,10 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log('Do you want an additional card')
+} else if (sum === 21) {
+    console.log('Congrats you have blackjack')
+} else {
+    console.log('Womp Womp')
+}
--- 3. Build a Blackjack game/16. Display the cards/index.js
+++ 3. Build a Blackjack game/16. Display the cards/index.js
@@ -7,10 +7,12 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 // 2. Store the cards paragraph in a variable called cardsEl
+let cardEl = document.getElementById('cards-el')
 
 function startGame() {
     // 3. Render the cars on the page using this format -> "Cards: 10 4"
     sumEl.textContent = "Sum: " + sum
+    cardEl.textContent = "Cards: " + firstCard + ' ' + secondCard
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
